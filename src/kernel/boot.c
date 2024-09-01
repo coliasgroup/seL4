88a47451b1af42ca2525c5fd8e5b3ce51e90fe26
Nick Spinale <nick@nickspinale.com>
2024-08-31 23:13:07 -0700

    passes

--- src/kernel/boot.c
+++ src/kernel/boot.c
@@ -969,18 +969,18 @@ BOOT_CODE bool_t init_freemem(word_t n_available, const p_region_t *available,
                 /* the region overlaps with the start of the available region.
                  * trim start of the available region */
                 avail_reg[a].start = MIN(avail_reg[a].end, reserved[r].end);
-                reserve_region(pptr_to_paddr_reg(reserved[r]));
+                // reserve_region(pptr_to_paddr_reg(reserved[r]));
                 r++;
             } else {
-                assert(reserved[r].start < avail_reg[a].end);
+                // assert(reserved[r].start < avail_reg[a].end);
                 /* take the first chunk of the available region and move
                  * the start to the end of the reserved region */
-                region_t m = avail_reg[a];
-                m.end = reserved[r].start;
-                insert_region(m);
+                // region_t m = avail_reg[a];
+                // m.end = reserved[r].start;
+                // insert_region(m);
                 if (avail_reg[a].end > reserved[r].end) {
                     avail_reg[a].start = reserved[r].end;
-                    reserve_region(pptr_to_paddr_reg(reserved[r]));
+                    // reserve_region(pptr_to_paddr_reg(reserved[r]));
                     r++;
                 } else {
                     a++;
